77438bda8930cdcededee9764cfddae5137fac50
Lauren Hetherington <laurenlhetherington@gmail.com>
2019-03-04 13:34:04 -0500

    Basic portal between two mostly empty rooms.

--- script/game.js
+++ script/game.js
@@ -4,6 +4,9 @@ var Game = {
     player: null,
     engine: null,
     fov: null,
+    portalFov: null,
+    portalFovZ: 0,
+    delta: [0,0],
     offset: [39, 13],
 
     init: function () {
@@ -17,10 +20,23 @@ var Game = {
             if (key in Game.map) {
                 return (Game.map[key].lightPasses());
             }
-            return false;
+            return true;
+        }
+
+        var lightPassesPortal = function (x, y) {
+            //return true;
+            if (!lightPasses(x+Game.delta[0],y+Game.delta[1])) {
+                return false;
+            }
+            var key = x + ',' + y + ',' + Game.portalFovZ;
+            if (key in Game.map) {
+                return (Game.map[key].lightPasses());
+            }
+            return true;
         }
 
         this.fov = new ROT.FOV.PreciseShadowcasting(lightPasses);
+        this.portalFov = new ROT.FOV.PreciseShadowcasting(lightPassesPortal);
         this._drawVisible();
         this.player.draw();
 
@@ -35,21 +51,28 @@ var Game = {
 
         // create some rooms. Third index is "dimension"
         //var roomSize=[6,6];
+        var colors=['#f0f',"#0ff"];
         for (let k = 0; k < 2; k++) { // dimension
             let roomSize=[6+k,6+k];
             for (let i = 0; i <= roomSize[0]; i++) { //x
                 for (let j = 0; j <= roomSize[1]; j++) {//y
                     let newKey = i + ',' + j + ',' + k;
                     if (!i || !j || i==roomSize[0] || j==roomSize[1]) {
-                        this.map[newKey] = new Tile('#','#fff',false,false,null);//'#';
+                        this.map[newKey] = new Tile('#',colors[k],false,false,null);//'#';
                     }
                     else {
-                        this.map[newKey] = new Tile('.','#fff',true,true,null);
+                        this.map[newKey] = new Tile('.',colors[k],true,true,null);
                         freeCells.push(newKey);
                     }
                 }
             }
         }
+
+        var pC=[0,2,0,7,4,1];
+        var portal = new Connection(pC[0],pC[1],pC[2],pC[3],pC[4],pC[5]);
+
+        this.map[portal.getKey(0)].contains=portal;
+        this.map[portal.getKey(1)].contains=portal;
 
         let index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
         let key = freeCells.splice(index, 1)[0];
@@ -65,7 +88,31 @@ var Game = {
         this.fov.compute(this.player.x, this.player.y, 100, function (x, y, r, visibility) {
             let key = x + ',' + y + ',' + Game.player.z;
             if (key in Game.map) {
+                if (Game.map[key].contains != null && Game.map[key].contains instanceof Connection) {
+                    Game._drawPortal(Game.map[key].contains);
+                }
                 Game.display.draw(x - Game.player.x + (Game.offset[0]), y - Game.player.y + (Game.offset[1]), Game.map[key].getChar(), Game.map[key].getColor());
+            }
+        });
+    },
+
+    _drawPortal: function(portal) {
+        //portalFovZ
+        //console.log("Draw portal called");
+        this.delta=portal.getDelta();
+        if (portal.p2[2] == this.player.z) {
+            this.portalFovZ = portal.p1[2];
+        }
+        else {
+            this.portalFovZ = portal.p2[2];
+            for (let i=0;i<this.delta.length;i++) {
+                this.delta[i]=-this.delta[i];
+            }
+        }
+        this.portalFov.compute(this.player.x-this.delta[0],this.player.y-this.delta[1],100, function (x,y,r,visibility) {
+            let key = x + ',' + y + ',' + Game.portalFovZ;
+            if (key in Game.map) {
+                Game.display.draw(x - Game.player.x + (Game.offset[0]+Game.delta[0]), y - Game.player.y + (Game.offset[1]+Game.delta[1]), Game.map[key].getChar(), Game.map[key].getColor());
             }
         });
     }
@@ -110,12 +157,28 @@ Player.prototype.handleEvent = function (e) {
 
     // check if valid
     let newKey = newX + ',' + newY + ',' + newZ;
+    //if (!(newKey in Game.map) || !(Game.map[newKey].passThrough())) {
     if (!(newKey in Game.map) || !(Game.map[newKey].passThrough())) {
         return;
+    }
+    if (Game.map[newKey].contains != null && Game.map[newKey].contains instanceof Connection) {
+        var whichSide;
+        if (newKey == Game.map[newKey].contains.getKey(0)) {
+            whichSide=1;
+        }
+        else {
+            whichSide=0;
+        }
+        let parts=Game.map[newKey].contains.getKey(whichSide).split(',');
+        newX=parseInt(parts[0]);
+        newY=parseInt(parts[1]);
+        newZ=parseInt(parts[2]);
+        //console.log("newZ?");
     }
     //Game.display.draw(this.x, this.y, Game.map[this.x + ',' + this.y]);
     this.x = newX;
     this.y = newY;
+    this.z = newZ;
     Game._drawVisible();
     this.draw();
     window.removeEventListener("keydown", this);
@@ -126,8 +189,8 @@ function Connection(x1,y1,z1,x2,y2,z2) {
     this.p1=[x1,y1,z1];
     this.p2=[x2,y2,z2];
 
-    this.getDelta = function(axis) {
-        return this.p2[axis] - this.p1[axis];
+    this.getDelta = function() {
+        return [this.p2[0] - this.p1[0], this.p2[1] - this.p1[1]];
     };
 
     this.getChar=function() {
@@ -137,6 +200,23 @@ function Connection(x1,y1,z1,x2,y2,z2) {
     this.getColor=function() {
         return '#00f';
     };
+
+    this.getKey=function(which) {
+        if (!which) {
+            return this.p1[0]+','+this.p1[1]+','+this.p1[2];
+        }
+        else {
+            return this.p2[0]+','+this.p2[1]+','+this.p2[2];
+        }
+    }
+
+    this.lightPasses=function() {
+        return true;
+    }
+
+    this.passThrough=function() {
+        return true;
+    }
 };
 
 function Tile(char,color,passable,seethrough,contains) {
@@ -146,25 +226,35 @@ function Tile(char,color,passable,seethrough,contains) {
     this.seethrough=seethrough;
     this.contains=contains;
     this.lightPasses=function() {
-        return this.seethrough;
+        if (this.contains==null) {
+            return this.seethrough;
+        }
+        else {
+            return this.contains.lightPasses();
+        }
     }
     this.passThrough=function() {
-        return this.passable;
+        if (this.contains==null) {
+            return this.passable;
+        }
+        else {
+            return this.contains.passThrough();
+        }
     }
     this.getChar=function() {
-        if (contains == null) {
+        if (this.contains == null) {
             return this.char;
         }
         else {
-            return contains.getChar();
+            return this.contains.getChar();
         }
     }
     this.getColor=function() {
-        if (contains == null) {
+        if (this.contains == null) {
             return this.color;
         }
         else {
-            return contains.getColor();
+            return this.contains.getColor();
         }
     }
 };
